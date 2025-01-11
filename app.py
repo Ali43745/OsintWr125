06320d223bd563cd2ca88d9d68124b2cd7ceb47c
Javeria78945577899 <javeria.15sep@gmail.com>
2025-01-11 15:47:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ else:
            FROM dbo_final_text1
            WHERE Downloaded_Image_Name = '{file_name_escaped}'
            """
-       result = pd.read_sql(query, engine)
+            result = pd.read_sql(query, engine)
             if not result.empty:
                 details = result.iloc[0].dropna().to_dict()  # Drop any columns with NaN values
                 return {key: value for key, value in details.items() if value != "Unknown"}
